d34c69f5818535a9278775c08aae70f94ee77eb1
i61979247 <talhaarik@std.iyte.edu.tr>
2020-11-16 09:49:38 +0000

    talha

---
+++ lab5/example5.py
@@ -0,0 +1,10 @@
+firstNumber = 1
+secondNumber = 1
+
+fibonacci = [firstNumber,secondNumber]
+for i in range(10):
+    firstNumber,secondNumber = secondNumber,firstNumber + secondNumber
+
+    fibonacci.append(secondNumber)
+
+print(fibonacci)
