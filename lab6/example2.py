480614ff2e55e141653b8f26ba2e3510b924f307
i61979247 <talhaarik@std.iyte.edu.tr>
2020-11-23 09:28:45 +0000

    talha

---
+++ lab6/example2.py
@@ -0,0 +1,6 @@
+midterm1 = int(input("midterm1: "))
+mideterm2 = int(input("midterm2: "))
+finalExam = int(input("final exam: "))
+
+avarage = (float(midterm1)*0.3) + (float(midterm2)*0.3) (float(finalExam)*0.4)
+print("avarage: {0}".format(avarage))
